2bccd1cdd1b2390de40349094771858a166ffa89
sameerafarvin <sameeraismaaeel@gmail.com>
2025-11-19 16:14:33 +0000

    added form in about app

--- blog/views.py
+++ blog/views.py
@@ -34,6 +34,7 @@ def post_detail(request, slug):
     # add form functionality
 
     if request.method == "POST":
+        #print("Received a POST request")
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
@@ -43,6 +44,7 @@ def post_detail(request, slug):
             messages.add_message(request, messages.SUCCESS,'Comment submitted and awaiting approval')
 
     comment_form = CommentForm()
+    #print("About to render template")
 
     return render(
         request,
